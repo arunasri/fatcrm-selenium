0ac0ab1f18d84e2a9d1fd782a8e83147e90a428c
Aruna Bolisetti <ruby.aruna28@gmail.com>
2017-10-15 13:11:05 -0400

    added dashboard elements

--- src/test/java/com/core/pageobjects/DashboardPO.java
+++ src/test/java/com/core/pageobjects/DashboardPO.java
@@ -1,5 +1,10 @@
 package com.core.pageobjects;
 
-public class DashboardPO {
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.How;
 
+public class DashboardPO {
+     @FindBy(how = How.ID, using = "Dashboard")
+     public WebElement dashboardTab;
 }
